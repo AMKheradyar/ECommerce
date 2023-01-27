65fdf9962582b1ef36fc8b7f0bdec60941a88646
AMKheradyar <AMKheradyar@gmail.com>
2023-01-27 14:59:58 +0330

    add login button url.

--- ECommerce/urls.py
+++ ECommerce/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('sell/', include('sell.urls'))
+    path('sell/', include('sell.urls')),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
