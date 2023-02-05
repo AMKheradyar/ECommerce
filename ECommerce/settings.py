86174346843b5911f7dbe0530c6e34bb69727eba
AMKheradyar <AMKheradyar@gmail.com>
2023-02-05 11:16:32 +0330

    change datebase from SQLite to PostgreSQL.

--- ECommerce/settings.py
+++ ECommerce/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ECommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'codebymeDB',
+        'USER': 'postgres',
+        'PASSWORD': 'xeradadmin',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
