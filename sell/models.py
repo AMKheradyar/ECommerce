fbdfc79d4ac2faca9256f38fada9931d5f304bfa
AMKheradyar <AMKheradyar@gmail.com>
2023-02-19 00:10:00 +0330

    update CHOISES fields with best practices.

--- sell/models.py
+++ sell/models.py
@@ -5,13 +5,17 @@ User = get_user_model()
 
 
 class Announcement(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name='announcement', default='1')
-    STATUS_STATES = (
-        ('A', "Accepted"),
-        ('P', "Pending"),
-        ('R', "Rejected")
+    STATUS_ACCEPTED = 'A'
+    STATUS_PENDING = 'P'
+    STATUS_REJECTED = 'R'
+
+    STATUS_CHOICES = (
+        (STATUS_ACCEPTED, "Accepted"),
+        (STATUS_PENDING, "Pending"),
+        (STATUS_REJECTED, "Rejected")
     )
-    status = models.CharField(max_length=1, choices=STATUS_STATES, default='P')
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name='announcement', default='1')
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='P')
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.FloatField()
